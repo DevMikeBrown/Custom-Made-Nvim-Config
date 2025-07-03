d8e87bebeb074cc582cb565b7dbd4567f4518f77
DevMikeBRown <uyandumuzi.dlamini@gmail.com>
2025-07-03 12:48:14 +0200

    C and C++ Compiling

--- init.lua
+++ init.lua
@@ -9,7 +9,9 @@ vim.opt.wrap = false
 vim.opt.relativenumber = true
 
 vim.cmd.colorscheme("catppuccin")
-require("catppuccin").setup({flavour = "macchiato"})
+require("catppuccin").setup({
+  flavour = "machiatto"
+})
 
 require("mason").setup()
 
@@ -223,6 +225,61 @@ end
 
 -- Optional keybind
 vim.keymap.set("n", "<F7>", build_and_run, { desc = "Build and Run C++" })
+
+local function BuildAndRunClanguage()
+  -- First, compile the program
+  vim.fn.jobstart({"gcc", "Main.c", "-o", "Main.exe"}, {
+    stdout_buffered = true,
+    stderr_buffered = true,
+
+    on_stdout = function(_, data, _)
+      if data then
+        for _, line in ipairs(data) do
+          if line ~= "" then
+            print("gcc: " .. line)
+          end
+        end
+      end
+    end,
+
+    on_stderr = function(_, data, _)
+      if data then
+        for _, line in ipairs(data) do
+          if line ~= "" then
+            print("gcc error: " .. line)
+          end
+        end
+      end
+    end,
+
+    on_exit = function(_, exit_code, _)
+      if exit_code == 0 then
+        print("✅ Compilation successful. Running Main.exe...")
+
+        -- Open new terminal buffer
+        vim.cmd("enew")
+        vim.bo.buftype = "nofile"
+        vim.bo.bufhidden = "wipe"
+
+        -- Run the executable
+        local job_id = vim.fn.termopen({"./Main.exe"}, {
+          on_exit = function(_, code, _)
+            vim.cmd("BufferLineCycleNext")
+          end
+        })
+
+        -- Save job ID if you want to track it later
+        vim.b.my_exe_job_id = job_id
+
+      else
+        print("❌ Compilation failed.")
+      end
+    end
+  })
+end
+
+-- Optional keybind
+vim.keymap.set("n", "<F8>", BuildAndRunClanguage, { desc = "Build and Run C" })
 
 vim.g.mapleader = ' '
 
